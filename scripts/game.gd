b02006699366eaf214eb4b822537e3f583efd6e2
cocosar07 <cocosar07370@gmail.com>
2017-08-26 22:27:31 +0200

    Allow to go back to building

--- scripts/game.gd
+++ scripts/game.gd
@@ -14,6 +14,7 @@ func _ready():
 	get_node("ui/buy_menu/ScrollContainer/VBoxContainer/Button").connect("pressed", get_node("player/tower_placer"), "set_tower", ["single_canon"])
 	get_node("ui/buy_menu/ScrollContainer/VBoxContainer/Button1").connect("pressed", get_node("player/tower_placer"), "set_tower", ["single_projectile"])
 	set_state(GameState.BUILDING)
+	set_process_input(true)
 
 func _process(delta):
 	counter -= delta
@@ -24,24 +25,25 @@ func _process(delta):
 
 func _input(event):
 	if event.type == InputEvent.MOUSE_BUTTON and event.is_pressed():
-		tower_placer.place_tower()
+		if current_state == GameState.BUILDING:
+			tower_placer.place_tower()
 	elif event.type == InputEvent.KEY and event.is_pressed():
-		set_state(GameState.DEFENDING)
+		if current_state == GameState.BUILDING:
+			set_state(GameState.DEFENDING)
+		else:
+			set_state(GameState.BUILDING)
 
 func set_state(state):
 	current_state = state
 	if current_state == GameState.BUILDING:
 		set_process(false)
-		set_process_input(true)
 		tower_placer.enable()
 		get_node("ui/buy_menu").show()
 		get_node("Main tower").disable()
 	elif current_state == GameState.DEFENDING:
 		set_process(true)
-		set_process_input(false)
 		tower_placer.disable()
 		while tower_placer.get_child_count() > 0:
 			tower_placer.remove_child(tower_placer.get_child(0))
-
 		get_node("ui/buy_menu").hide()
 		get_node("Main tower").enable()
