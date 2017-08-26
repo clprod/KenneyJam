50d99b773717a64b05a6dc5d737e5f30fd3cd5eb
cocosar07 <cocosar07370@gmail.com>
2017-08-26 17:04:26 +0200

    Changed spawn enemy counter

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,8 +6,8 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	counter += delta
-	if counter >= 1:
+	counter -= delta
+	if counter <= 0:
 		var enemy = load("res://scenes/enemies/enemy_weak.tscn").instance()
 		get_node("map/Path2D").add_child(enemy)
-		counter = 0
+		counter = 1
