b54ca9fcc64f53fd1f1bdb28bef752cd7daa296b
cocosar07 <cocosar07370@gmail.com>
2017-08-26 22:21:45 +0200

    Added buy menu

--- scripts/game.gd
+++ scripts/game.gd
@@ -11,6 +11,8 @@ var current_state
 onready var tower_placer = get_node("player/tower_placer")
 
 func _ready():
+	get_node("ui/buy_menu/ScrollContainer/VBoxContainer/Button").connect("pressed", get_node("player/tower_placer"), "set_tower", ["single_canon"])
+	get_node("ui/buy_menu/ScrollContainer/VBoxContainer/Button1").connect("pressed", get_node("player/tower_placer"), "set_tower", ["single_projectile"])
 	set_state(GameState.BUILDING)
 
 func _process(delta):
@@ -22,13 +24,7 @@ func _process(delta):
 
 func _input(event):
 	if event.type == InputEvent.MOUSE_BUTTON and event.is_pressed():
-		if tower_placer.get_child_count() == 0:
-			return
-		var tower = tower_placer.get_child(0)
-		var tower_pos = tower.get_global_pos()
-		tower_placer.remove_child(tower)
-		get_node("towers").add_child(tower)
-		tower.set_global_pos(tower_pos)
+		tower_placer.place_tower()
 	elif event.type == InputEvent.KEY and event.is_pressed():
 		set_state(GameState.DEFENDING)
 
@@ -38,6 +34,7 @@ func set_state(state):
 		set_process(false)
 		set_process_input(true)
 		tower_placer.enable()
+		get_node("ui/buy_menu").show()
 		get_node("Main tower").disable()
 	elif current_state == GameState.DEFENDING:
 		set_process(true)
@@ -45,4 +42,6 @@ func set_state(state):
 		tower_placer.disable()
 		while tower_placer.get_child_count() > 0:
 			tower_placer.remove_child(tower_placer.get_child(0))
+
+		get_node("ui/buy_menu").hide()
 		get_node("Main tower").enable()
--- scripts/tower_placer.gd
+++ scripts/tower_placer.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export(int) var tile_size
+export(NodePath) var tower_parent
 
 func _process(delta):
 	var pos = get_global_mouse_pos()
@@ -13,6 +14,23 @@ func _process(delta):
 
 func enable():
 	set_process(true)
+	show()
 
 func disable():
 	set_process(false)
+	hide()
+
+func place_tower():
+	if get_child_count() == 0:
+		return
+	var tower = get_child(0)
+	remove_child(tower)
+	get_node(tower_parent).add_child(tower)
+	tower.set_pos(get_pos())
+
+func set_tower(tower_file):
+	if get_child_count() == 1:
+		remove_child(get_child(0))
+	
+	var tower = load("res://scenes/towers/" + tower_file + ".tscn").instance()
+	add_child(tower)
