6a8d34bc78e99cdd38424d71653235821d423666
cocosar07 <cocosar07370@gmail.com>
2017-08-26 18:06:18 +0200

    Added projectile and enemies collisions

--- scripts/projectile_base.gd
+++ scripts/projectile_base.gd
@@ -7,6 +7,13 @@ func _ready():
 
 func _fixed_process(delta):
 	move(velocity * delta)
+	if get_global_pos().x <= 0 or get_global_pos().x >= get_viewport_rect().size.width or get_global_pos().y <= 0 or get_global_pos().y >= get_viewport_rect().size.height:
+		queue_free()
+	else:
+		if is_colliding():
+			if get_collider().get_parent().has_method("take_damages"):
+				get_collider().get_parent().take_damages(1)
+				queue_free()
 
 func shoot(direction, speed):
 	velocity = direction * speed
