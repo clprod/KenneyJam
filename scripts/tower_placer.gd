02e6f706783a5c5e6dd2287b55f8d7bf24003c5b
cocosar07 <cocosar07370@gmail.com>
2017-08-26 22:39:57 +0200

    Tower map test

--- scripts/tower_placer.gd
+++ scripts/tower_placer.gd
@@ -11,6 +11,22 @@ func _process(delta):
 	pos *= tile_size
 	pos += Vector2(tile_size, tile_size)/2
 	set_pos(pos)
+	
+	if get_child_count() == 0:
+		return
+	if is_place_valid():
+		set_tower_modulate(Color(0, 1, 0, 0.5))
+	else:
+		set_tower_modulate(Color(1, 0, 0, 0.5))
+
+func is_place_valid():
+	var map = get_node("/root/game/map/TileMap")
+	return map.get_cellv(get_pos()/tile_size) == 2
+
+func set_tower_modulate(color):
+	var tower = get_child(0)
+	for child in tower.get_children():
+		child.set_modulate(color)
 
 func enable():
 	set_process(true)
@@ -23,7 +39,10 @@ func disable():
 func place_tower():
 	if get_child_count() == 0:
 		return
+	if not is_place_valid():
+		return
 	var tower = get_child(0)
+	set_tower_modulate(Color(1, 1, 1, 1))
 	remove_child(tower)
 	get_node(tower_parent).add_child(tower)
 	tower.set_pos(get_pos())
