556b47ab84129a09274aa669024ad85c531d5b1f
cocosar07 <cocosar07370@gmail.com>
2017-08-27 00:22:20 +0200

    Faster explosion screen shake

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -4,7 +4,7 @@ func _ready():
 	set_process(true)
 	get_node("AnimationPlayer").play("diappear")
 	set_rotd(rand_range(0, 360))
-	get_node("/root/game/Camera2D").shake(0.3, 100, 3)
+	get_node("/root/game/Camera2D").shake(0.1, 100, 3)
 
 func _process(delta):
 	if not get_node("AnimationPlayer").is_playing():
