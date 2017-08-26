e316166aa556c4aeec6241e047bffd7a13c77a96
cocosar07 <cocosar07370@gmail.com>
2017-08-26 16:24:13 +0200

    Towers now destroy enemies

--- scripts/enemy_base.gd
+++ scripts/enemy_base.gd
@@ -1,15 +1,19 @@
-extends Node2D
+extends PathFollow2D
 
-export(int) var health = 10
+export(int) var health = 5
 export(float) var speed = 10
 
-onready var path_follow = get_parent()
 var end_threshold = 5
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	path_follow.set_offset(path_follow.get_offset() + (speed * delta))
-	if abs(path_follow.get_offset() - path_follow.get_parent().get_curve().get_baked_length()) <= end_threshold:
+	set_offset(get_offset() + (speed * delta))
+	if abs(get_offset() - get_parent().get_curve().get_baked_length()) <= end_threshold:
+		queue_free()
+
+func take_damages(amount):
+	health -= amount
+	if health < 0:
 		queue_free()
--- scripts/tower_base.gd
+++ scripts/tower_base.gd
@@ -5,10 +5,14 @@ export(int) var shooting_power
 export(float) var shooting_speed
 export(int) var cost
 
+var last_shoot = 0
+
 func _ready():
 	set_process(true)
 
 func _process(delta):
+	last_shoot -= delta
+	
 	var enemies = get_tree().get_nodes_in_group("enemies")
 	var target = null
 	for enemy in enemies:
@@ -18,3 +22,6 @@ func _process(delta):
 
 	if target != null:
 		get_node("canon").look_at(target.get_global_pos())
+		if last_shoot <= 0:
+			target.take_damages(1)
+			last_shoot = shooting_speed
